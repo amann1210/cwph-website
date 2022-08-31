68dd04459713be79a136b3b1a7d089521ba799dc
Aman Agrawal <66276113+amann1210@users.noreply.github.com>
2022-08-31 13:34:40 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth , GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiMNuxyVMcAaj_aMvKznCs19MS7X_DdjU",
+apiKey: "AIzaSyBiMNuxyVMcAaj_aMvKznCs19MS7X_DdjU",
   authDomain: "cwph-lnmiit.firebaseapp.com",
   projectId: "cwph-lnmiit",
   storageBucket: "cwph-lnmiit.appspot.com",
   messagingSenderId: "313567962778",
   appId: "1:313567962778:web:58925a37c7b725edcda123",
-  measurementId: "G-69TP74R4HS" 
+  measurementId: "G-69TP74R4HS"
 };
 
 const app = initializeApp(firebaseConfig);
